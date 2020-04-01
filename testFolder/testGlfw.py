9cfa2e73f477db159789ac9dcc30b956dfd0fb24
arduinocc04 <arduinocc04@gmail.com>
2020-04-01 23:07:27 +0900

    whoo

---
+++ testFolder/testGlfw.py
@@ -0,0 +1,59 @@
+import glfw
+from OpenGL.GL import *
+import numpy as np
+from math import sin, cos
+
+if not glfw.init():
+    raise Exception("glfw can not be initialized!")
+
+
+window = glfw.create_window(1280, 720, "My OpenGL window", None, None)
+
+if not window:
+    glfw.terminate()
+    raise Exception("glfw window can not be created!")
+
+glfw.set_window_pos(window, 400, 200)
+
+# make the context current
+glfw.make_context_current(window)
+```
+vertices = [-0.5, -0.5, 0.0,
+             0.5, -0.5, 0.0,
+             0.0,  0.5, 0.0]
+
+colors = [1.0, 0.0, 0.0,#red
+          0.0, 1.0, 0.0,#green
+          0.0, 0.0, 1.0]#blue
+
+vertices = np.array(vertices, dtype=np.float32)
+colors = np.array(colors, dtype=np.float32)
+
+
+glEnableClientState(GL_VERTEX_ARRAY)
+glVertexPointer(3, GL_FLOAT, 0, vertices)
+
+glEnableClientState(GL_COLOR_ARRAY)
+glColorPointer(3, GL_FLOAT, 0, colors)
+
+glClearColor(0, 0.1, 0.1, 1)
+
+while not glfw.window_should_close(window):
+    glfw.poll_events()
+
+    glClear(GL_COLOR_BUFFER_BIT)
+
+    ct = glfw.get_time()  # returns the elapsed time, since init was called
+
+    glLoadIdentity()
+    glScale(abs(sin(ct)), abs(sin(ct)), 1)
+    glRotatef(tan(ct) * 45, 0, 0, 1)
+    glTranslatef(sin(ct), cos(ct), 0)
+
+    glDrawArrays(GL_TRIANGLES, 0, 3)
+
+    glfw.swap_buffers(window)
+
+
+
+glfw.terminate()
