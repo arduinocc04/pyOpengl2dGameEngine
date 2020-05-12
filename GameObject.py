fc16e14a2fdaeb443ae35cbfb78463be2c2816f1
arduinocc04 <arduinocc04@gmail.com>
2020-05-12 23:12:18 +0900

    apply renederer, soundSystem, healthSystem, moveSystem, trigger.

--- GameObject.py
+++ GameObject.py
@@ -3,9 +3,16 @@ import numpy as np
 
 class Actor:
     def __init__(self, coordinate):
-
+        #this are components. you can what that you want
         self.collider = False
-        self.colliderSetting = 'n'
+        self.colliderSetting = 'n'#n->None, g->ground, c->character. usage: 'n' or 'gc' or 'cg' or 'g' 'c'
+
+        self.renderer = False
+        self.soundSystem = False
+        self.healthSystem = False
+        self.mover = False
+        self.trigger = False
+        self.triggerSetting = 'n'#n->None, p->playerOnly, c->characterOnly. usage: 'n' or 'pc' or 'cp' or 'p' or 'c'
 
         self.coordinate = np.array([float(coordinate[0]), float(coordinate[1])])
     
@@ -15,6 +22,8 @@ class Actor:
         pass
     def collided(self):
         pass
+    def triggerCollided(self, collidedObj):
+        pass
 
 
 class Ground:
@@ -22,6 +31,7 @@ class Ground:
         self.componentList = []
         self.angle = angle
         self.coordinate = np.array(coordinate)
+        self.friction = 1
 
 class BackGround:
     def __init__(self):
